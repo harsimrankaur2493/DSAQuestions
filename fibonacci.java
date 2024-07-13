06e4d39c9f5cb98af5f6a1900e645be13caad2c7
harsimrankaur2493 <162671426+harsimrankaur2493@users.noreply.github.com>
2024-07-13 13:00:41 +0530

    Add files via upload
    fibonacci sequence using reccursion

---
+++ fibonacci.java
@@ -0,0 +1,18 @@
+public class fibonacci {
+    
+   public static void fib(int a, int b, int n){
+        if(n==0){
+            return;
+        }
+
+        int c=a+b;
+        System.out.println(c);
+        fib(b,c,n-1);    
+   }
+
+   public static void main(String []args){
+    System.out.println(0);
+    System.out.println(1);
+    fib(0,1,9);
+   }
+}
