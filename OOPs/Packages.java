e28fe9466337ac98a509860427621eebda72d192
Harsimran Kaur <162671426+harsimrankaur2493@users.noreply.github.com>
2024-07-28 10:54:01 +0530

    Create Packages.java

---
+++ OOPs/Packages.java
@@ -0,0 +1,8 @@
+import java.util.*; //package called in order to use scanner class
+public class Packages {
+    //package definition: group of similar kind of classes interfaces and sub ackages
+    //2 types : inbuilt packages and user defined 
+    public static void main(String []args){
+        Scanner sc= new Scanner(System.in); //=> class of util package
+    }
+}
