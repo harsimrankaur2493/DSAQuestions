dc536d8b31d84b6a08c18b9a566fd5f1718d19e6
Harsimran Kaur <162671426+harsimrankaur2493@users.noreply.github.com>
2024-08-13 09:03:42 +0530

    Create FindIndex.java
    Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.

---
+++ FindIndex.java
@@ -0,0 +1,14 @@
+class FindIndex {
+    public int strStr(String haystack, String needle) {
+        /**
+         * use indexOf
+         * haystack.indexOf(needle);
+         */
+        int index=haystack.indexOf(needle);
+        return index;
+    }
+}
+
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
