78104c206dde890d3300fbcc81a95db50f1df8d3
Harsimran Kaur <162671426+harsimrankaur2493@users.noreply.github.com>
2024-08-25 12:21:27 +0530

    Create creation.java

---
+++ linked-list/creation.java
@@ -0,0 +1,15 @@
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class creation{
+    public static void main(String []args){
+        //empty linked list
+        LinkedList list= new LinkedList();
+
+        //linked list with a collection 'C'
+        LinkedList list1= new LinkedList(C);
+
+       
+
+    }
+}
