7d686518e1248b09f38e86a31220186dfa683bf1
Harsimran Kaur <162671426+harsimrankaur2493@users.noreply.github.com>
2024-09-12 14:33:33 +0530

    Time: 2 ms (52.06%) | Memory: 41.1 MB (94.10%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        if(s.length()%2!=0){
+            return false;
+        }
+        Stack<Character> st = new Stack<>();
+        for(char c: s.toCharArray()){
+            if(c=='{'){
+                st.push('}');
+            }
+            else if(c=='('){
+                st.push(')');
+            }
+            else if(c=='['){
+                st.push(']');
+            }
+            else{
+                if( st.isEmpty() ||st.pop()!=c){
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+        
+    }
+}
