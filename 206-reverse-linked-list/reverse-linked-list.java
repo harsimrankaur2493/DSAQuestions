81916ee1ce3edb95dd49aa9dc68e443243433ce0
Harsimran Kaur <162671426+harsimrankaur2493@users.noreply.github.com>
2025-02-18 06:56:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (18.94%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -9,25 +9,16 @@
  * }
  */
 class Solution {
-    public ListNode reverseList(ListNode head) {
+    public ListNode reverseList(ListNode head){
 
-        if(head==null){
-            return null;
+        if(head==null || head.next==null){
+            return head;
         }
 
-        ListNode prev = null;
-        ListNode curr=head;
-        ListNode Next;
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
 
-        while(curr != null){
-            Next = curr.next;
-            curr.next = prev;
-
-            prev = curr;
-            curr= Next;
-        }
-        head = prev;
-
-        return head;
+        return newHead;
     }
 }
